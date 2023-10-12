d63e16577f9d5f1264557010c38677c4450f9292
muandane <zmoualhi@outlook.com>
2023-10-12 19:58:38 +0100

    Brew formula update for azureprice version v0.0.1-rc1

--- azureprice.rb
+++ azureprice.rb
@@ -9,7 +9,7 @@ class Azureprice < Formula
 
   on_macos do
     url "https://github.com/muandane/azureprice/releases/download/v0.0.1-rc1/azureprice_0.0.1-rc1_darwin_all.tar.gz"
-    sha256 "463167a370117963490ecdddc7123f1e35c54c1380d86527d217b114f4020b56"
+    sha256 "f3796884dc76c4dc49c4e280b7c05fb5e14273e876f57fdaf0a3b039fe20182c"
 
     def install
       bin.install "azureprice"
@@ -19,7 +19,7 @@ class Azureprice < Formula
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/muandane/azureprice/releases/download/v0.0.1-rc1/azureprice_0.0.1-rc1_linux_amd64.tar.gz"
-      sha256 "a5f2996b46eabd71a81a1dc245bf60f9a59a9da26671289abe42e00cf53e8546"
+      sha256 "2888528cbe78fa5b236d7e1178911d4c7f72045c1d44b6649f8a05cfb3929f9c"
 
       def install
         bin.install "azureprice"
@@ -27,7 +27,7 @@ class Azureprice < Formula
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/muandane/azureprice/releases/download/v0.0.1-rc1/azureprice_0.0.1-rc1_linux_arm64.tar.gz"
-      sha256 "5ddb927aaa04d57f5345552e892bc210271ceea6a831a94adaf8c922666dde2b"
+      sha256 "34e1c44416536fe110c3ece83b5a137b88387665bfeecf9d8c15d44e27c7d10a"
 
       def install
         bin.install "azureprice"
